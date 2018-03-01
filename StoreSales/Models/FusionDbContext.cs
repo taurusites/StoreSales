5ef2eb3eb9b8604ac8de8c3a15cacbf08d588bc9
wcorreia <william@taurusites.com>
2018-03-01 18:22:12 +0530

    First Commit

---
+++ StoreSales/Models/FusionDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace StoreSales.Models
+{
+    public partial class FusionDbContext : DbContext
+    {
+        public FusionDbContext() : base("name=DefaultConnection") { }
+    }
+}
