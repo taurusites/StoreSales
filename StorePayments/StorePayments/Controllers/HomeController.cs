400d6087e7e11ab4a840df43d50f0caece315f35
taurusites <william@taurusites.com>
2018-03-02 16:33:24 +0530

    Commit After small changes

--- StorePayments/StorePayments/Controllers/HomeController.cs
+++ StorePayments/StorePayments/Controllers/HomeController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace StorePayments.Models
+
+namespace StorePayments.Controllers
 {
     public class HomeController : Controller
     {
---
+++ StorePayments/StorePayments/Models/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace StorePayments.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("name=DefaultConnection") { }
+    }
+}
